69611a430c52d9cbdda635be19bd2471ce2fd2a5
Kevin Matz <kevin@winchelseasystems.com>
2023-11-04 16:23:36 -0700

    Adding data annotations and custom error messages

--- RazorPagesMovie/Migrations/20231104232110_New_DataAnnotations.Designer.cs
+++ RazorPagesMovie/Migrations/20231104232110_New_DataAnnotations.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RazorPagesMovie.Data;
 
@@ -10,9 +11,11 @@ using RazorPagesMovie.Data;
 namespace RazorPagesMovie.Migrations
 {
     [DbContext(typeof(RazorPagesMovieContext))]
-    partial class RazorPagesMovieContextModelSnapshot : ModelSnapshot
+    [Migration("20231104232110_New_DataAnnotations")]
+    partial class New_DataAnnotations
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "7.0.13");
--- RazorPagesMovie/Models/Movie.cs
+++ RazorPagesMovie/Models/Movie.cs
@@ -6,15 +6,27 @@ namespace RazorPagesMovie.Models;
 public class Movie
 {
     public int Id { get; set; }
+
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "The title must be at least three characters long.")]
+    [Required]
     public string? Title { get; set; }
 
-    [Display(Name = "Release Date")]
+    // [Display(Name = "Release Date")]
     [DataType(DataType.Date)]
     public DateTime ReleaseDate { get; set; }
-    public string? Genre { get; set; }
 
+    [Range(1, 100)]
+    [DataType(DataType.Currency)]
     [Column(TypeName = "decimal(18, 2)")]
     public decimal Price { get; set; }
 
+    [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$", ErrorMessage = "Please ensure the first letter is a capital letter. Special characters may not be supported.")]
+    [Required]
+    [StringLength(30)]
+    public string? Genre { get; set; }
+
+    [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$", ErrorMessage = "Please ensure the first letter is a capital letter. Special characters may not be supported.")]
+    [StringLength(5)]
+    [Required]
     public string Rating { get; set; } = string.Empty;
 }
